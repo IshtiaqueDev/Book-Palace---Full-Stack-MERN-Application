bf9712c2db65351a3427797704678960e3b73bc9
IshtiaqueDev <javedjanijaved810@gmail.com>
2026-09-03 23:40:17 +0500

    Added Origin in Cors

--- Backend/app.js
+++ Backend/app.js
@@ -76,6 +76,7 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+
+app.listen(port, "0.0.0.0",() => {
   console.log(`Server listening on port ${port}`);
 });
